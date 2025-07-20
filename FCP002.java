2450dc3f87ad45949277b3313c2e5f7a9655ec9f
XcelevatorKarthik <karthikeyan.2512@xcelevate.org>
2025-07-20 13:07:44 +0530

    FCP002

---
+++ FCP002.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class FCP002{
+   public static void main(String []args){
+     Scanner kk=new Scanner(System.in);
+     int a=kk.nextInt();
+      System.out.println(a);
+
+}
+}
